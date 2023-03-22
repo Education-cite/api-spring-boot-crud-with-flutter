9cbfed9db517601b3d44bc6b5b81d6af916c05e2
Education-cite <tusarjobs@gmail.com>
2023-03-22 20:05:04 +0600

    Initial commit

---
+++ src/main/java/com/flutter/crud/apispringbootcrud/entity/User.java
@@ -0,0 +1,4 @@
+package com.flutter.crud.apispringbootcrud.entity;
+
+public class User {
+}
---
+++ src/test/java/com/flutter/crud/apispringbootcrud/ApiSpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flutter.crud.apispringbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
