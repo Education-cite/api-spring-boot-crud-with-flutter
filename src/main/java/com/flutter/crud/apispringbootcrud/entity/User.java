4914a1fcf60025f54e9b7c84a6467433071fc0a0
Education-cite <tusarjobs@gmail.com>
2023-03-22 20:15:43 +0600

    fff

--- src/main/java/com/flutter/crud/apispringbootcrud/entity/User.java
+++ src/main/java/com/flutter/crud/apispringbootcrud/entity/User.java
@@ -1,4 +1,39 @@
 package com.flutter.crud.apispringbootcrud.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="User")
 public class User {
+
+    @Id
+    @GeneratedValue
+
+    private int id;
+    private String name;
+
+    private String email;
+
+    private String address;
+
+
+
+
+
+
+
+
+
+
 }
